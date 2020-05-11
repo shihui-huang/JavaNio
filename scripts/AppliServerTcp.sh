4b902b6dbcd5b7a0957c130a6c92f58a3c2776d7
shihui0328 <huangshihui0328@outlook.com>
2020-05-11 21:14:35 +0200

    feat: add scripts for the applis
    for easier compilation

---
+++ scripts/AppliServerTcp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+MAVEN_REPOS=${HOME}/.m2/repository
+LOG4J_JAR=${MAVEN_REPOS}/log4j/log4j/1.2.17/log4j-1.2.17.jar
+
+PROJECTROOT=~/Documents/CSC/CSC4509/DM/csc4509-dm/
+
+export CLASSPATH=$LOG4J_JAR:$PROJECTROOT/target/classes
+
+java tsp.csc4509.dm.appli.AppliServerTcp $*
