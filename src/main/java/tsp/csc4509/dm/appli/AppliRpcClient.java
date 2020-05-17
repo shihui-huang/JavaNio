9134350ed95fefff185935150b13fb61546de5c6
shihui0328 <huangshihui0328@outlook.com>
2020-05-17 21:09:46 +0200

    feat(applirpcclient): add use of LIST

--- src/main/java/tsp/csc4509/dm/appli/AppliRpcClient.java
+++ src/main/java/tsp/csc4509/dm/appli/AppliRpcClient.java
@@ -3,6 +3,7 @@ package tsp.csc4509.dm.appli;
 import tsp.csc4509.dm.mandelbrot.Complexe;
 import tsp.csc4509.dm.rpc.RpcClient;
 import tsp.csc4509.dm.rpc.RpcParam;
+import tsp.csc4509.dm.rpc.RpcType;
 import tsp.csc4509.dm.rpcparam.AdditionParam;
 import tsp.csc4509.dm.rpc.RpcReply;
 import tsp.csc4509.dm.rpcparam.MultiplicationComplexeParam;
@@ -24,54 +25,66 @@ public class AppliRpcClient {
          */
         private static final int PORTARG = 1;
         /**
-         * position du port du type du param dans argv[].
+         * position du port du id du rpctype dans argv[].
          */
-        private static final int PARAMARG = 2;
+        private static final int IDARG = 2;
+         /**
+          * position du port du type du param dans argv[].
+          */
+    private static final int PARAMARG = 3;
 
     public static void main(final String[] argv) throws IOException, ClassNotFoundException {
         // Vérifier que le main reçoit bien 4 arguments
-        if (argv.length < 4) {
-            System.out.println("usage: java AppliRpcClient <machine> <port> <param> <val1> <val2> (<val3> <val4>)");
-            System.out.println("param: add or multipli. add: val1 + val2. multipli: (val1 + i val2) * (val3 + i val4)");
+        if (argv.length < 3) {
+            System.out.println("usage: java AppliRpcClient <machine> <port> <id> (<param> <val1> <val2> (<val3> <val4>))");
+            System.out.println("id: LIST/AVERAGELOAD/COMPUTELIST/COMPUTE");
+            System.out.println("Param only exists when id is COMPUTE。 param: add/multipli. add: val1 + val2. multipli: (val1 + i val2) * (val3 + i val4)");
             return;
         }
 
         String serverHost = argv[HOSTARG];
         int serverPort = Integer.parseInt(argv[PORTARG]);
-        String param = argv[PARAMARG];
+        String id = argv[IDARG];
 
-        switch (param) {
-            case "add" :
-                int valInt1 = Integer.parseInt(argv[3]);
-                int valInt2 = Integer.parseInt(argv[4]);
-                AdditionParam additionParam = new AdditionParam(valInt1, valInt2);
+        if(id.equals("COMPUTE")){
+            String param = argv[PARAMARG];
 
-                RpcClient rpcClientAdd = new RpcClient(serverHost, serverPort, "Add", (Class<? extends RpcParam>) additionParam.getResultClass(), additionParam);
-                Serializable resultAdd = rpcClientAdd.getResult();
-                System.out.println(resultAdd);
-                break;
+            switch (param) {
+                case "add" :
+                    int valInt1 = Integer.parseInt(argv[3]);
+                    int valInt2 = Integer.parseInt(argv[4]);
+                    AdditionParam additionParam = new AdditionParam(valInt1, valInt2);
 
-            case "multipli":
-                if (argv.length != NBARGS) {
-                    System.out.println("usage: java AppliRpcClient <machine> <port> <param> <val1> <val2> (<val3> <val4>)");
+                    RpcClient rpcClientAdd = new RpcClient(serverHost, serverPort, "COMPUTE", (Class<? extends RpcParam>) additionParam.getResultClass(), additionParam);
+                    Serializable resultAdd = rpcClientAdd.getResult();
+                    System.out.println(resultAdd);
+                    break;
+
+                case "multipli":
+                    if (argv.length != NBARGS) {
+                        System.out.println("usage: java AppliRpcClient <machine> <port> <param> <val1> <val2> (<val3> <val4>)");
+                        System.out.println("param: add or multipli. add: val1 + val2. multipli: (val1 + i val2) * (val3 + i val4)");
+                        return;
+                    }
+                    double valReal1 = Double.parseDouble(argv[3]);
+                    double valImagin1 = Double.parseDouble(argv[4]);
+                    double valReal2 = Double.parseDouble(argv[5]);
+                    double valImagin2 = Double.parseDouble(argv[6]);
+                    Complexe complex1 = new Complexe(valReal1,valImagin1);
+                    Complexe complexe2 = new Complexe(valReal2,valImagin2);
+                    MultiplicationComplexeParam multipliParam = new MultiplicationComplexeParam (complex1,complexe2);
+
+                    RpcClient rpcClientMultipli = new RpcClient(serverHost, serverPort, "COMPUTE", (Class<? extends RpcParam>) multipliParam.getResultClass(), multipliParam);
+                    Serializable resultMultipli = rpcClientMultipli.getResult();
+                    System.out.println(resultMultipli);
+                    break;
+                default:
                     System.out.println("param: add or multipli. add: val1 + val2. multipli: (val1 + i val2) * (val3 + i val4)");
                     return;
-                }
-                double valReal1 = Double.parseDouble(argv[3]);
-                double valImagin1 = Double.parseDouble(argv[4]);
-                double valReal2 = Double.parseDouble(argv[5]);
-                double valImagin2 = Double.parseDouble(argv[6]);
-                Complexe complex1 = new Complexe(valReal1,valImagin1);
-                Complexe complexe2 = new Complexe(valReal2,valImagin2);
-                MultiplicationComplexeParam multipliParam = new MultiplicationComplexeParam (complex1,complexe2);
-
-                RpcClient rpcClientMultipli = new RpcClient(serverHost, serverPort, "Multipli", (Class<? extends RpcParam>) multipliParam.getResultClass(), multipliParam);
-                Serializable resultMultipli = rpcClientMultipli.getResult();
-                System.out.println(resultMultipli);
-                break;
-            default:
-                System.out.println("param: add or multipli. add: val1 + val2. multipli: (val1 + i val2) * (val3 + i val4)");
-                return;
+            }
+        }else if (id.equals("LIST")){
+            RpcClient rpcClient = new RpcClient(serverHost, serverPort, id);
+            Serializable result = rpcClient.getResult();
         }
     }
 }
